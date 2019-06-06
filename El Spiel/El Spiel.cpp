a25d3afdc23248cf24555719c576acee1f5aee8e
Timo Nitsch <11011218@stud.hochschule-heidelberg.de>
2019-06-06 10:12:11 +0200

    Projektdateien hinzufügen.

---
+++ El Spiel/El Spiel.cpp
@@ -0,0 +1,23 @@
+// El Spiel.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <iostream>
+#include "raylib.h"
+
+
+
+
+int main()
+{
+    
+	InitWindow(500, 500, "El Spiel");
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		EndDrawing();
+	}
+
+	CloseWindow();
+}
