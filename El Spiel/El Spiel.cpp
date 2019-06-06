c7dcc4a52a03354b72dce202cee88e62fb0fe2e0
Timo Nitsch <11011218@stud.hochschule-heidelberg.de>
2019-06-06 16:41:43 +0200

    .

--- El Spiel/El Spiel.cpp
+++ El Spiel/El Spiel.cpp
@@ -10,13 +10,16 @@
 
 int main()
 {
-    
 	InitWindow(500, 500, "El Spiel");
-
+	int screenHeight = GetMonitorHeight(0);
+	int screenWidth = GetMonitorWidth(0);
+	SetWindowSize(screenWidth, screenHeight);
+	//ToggleFullscreen();
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
+		DrawText("El Spiel, Alla!", 100, 100, 20, BLACK);
 		EndDrawing();
 	}
 
